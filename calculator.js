8a584e4165464891a248a47a984b30697a0226b5
harshitupreti101@gmail.com <harshiturpeti101>
2025-09-03 15:31:26 +0530

    ui done

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const resultBtn = document.getElementsByClassName("orange-btn");
+const input = document.getElementsByClassName("calculation-section");
+addEventListener('click', (e)=>{
+    var s = input.target.value();
+    for (const val of s) {
+        
+    }
+})
