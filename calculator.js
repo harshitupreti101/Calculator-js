f94f5bf720327df5fce9cff7c1895679e17f88c2
harshitupreti101@gmail.com <harshiturpeti101>
2025-09-04 17:10:35 +0530

    complete

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
-const resultBtn = document.getElementsByClassName("orange-btn");
-const input = document.getElementsByClassName("calculation-section");
-addEventListener('click', (e)=>{
-    var s = input.target.value();
-    for (const val of s) {
-        
-    }
+const button = document.querySelectorAll("button");
+var input = document.querySelector(".calculation-section");
+
+console.log(input);
+var s = "";
+var arr = Array.from(button);
+arr.forEach(button => {
+    button.addEventListener('click',(e)=>{
+        // .target unhi element mai lage skte hai jo ekk current event hooo.
+        var val = e.target.innerHTML;
+        if (val == "=") {
+            s = eval(s);
+        }
+        else if (val == "DEL") {
+            s = s.substring(0,s.length-1);
+        }
+        else if (val == "AC") {
+            s = "";
+        }
+        else {
+            s += val;
+        }
+        input.value = s;
+    })
 })
+
